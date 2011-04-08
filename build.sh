ad85afa23788fd7f257ee85e153d4935ae6e4712
Tim Green <iamtimgreen@gmail.com>
2011-04-08 12:52:19 +0800

    add test cook config files

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ ROOT=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 LIB=$ROOT/lib
 SRC=$ROOT/src
+TEST=$ROOT/test
 GEN_DIR=$ROOT/cook_gen
 BUILD_DIR=$ROOT/cook_build
 BIN_DIR=$ROOT/cook_bin
@@ -22,12 +23,27 @@ generateParser() {
   mkBuildDir
   mkdir -p $GEN_DIR/cook/parser/
   cd $GEN_DIR/cook/parser/
-  java -cp $CP mouse.Generate -G $SRC/cook/parser/cook.peg -p cook.parser -P Parser -S Semantics
+  java -cp $CP mouse.Generate -G $SRC/cook/parser/cook.peg -p cook.parser -P Parser
   cd $ROOT
 }
 
-main() {
+buildParser() {
   generateParser
+  javac -d $BUILD_DIR -cp $CP $GEN_DIR/cook/parser/Parser.java
+}
+
+testParser() {
+  buildParser
+
+  IFS="
+"
+  for TEST_CASE in $(ls $TEST/*.cook); do
+    java -cp $CP:$BUILD_DIR mouse.TryParser -P cook.parser.Parser -f $TEST_CASE
+  done
+}
+
+main() {
+  testParser
 }
 
 main
