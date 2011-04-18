ac778fcd14ca8ecdbebfac634525fbb40137d905
Tim Green <iamtimgreen@gmail.com>
2011-04-18 22:47:43 +0800

    Refactor FileUtil

--- src/cook/util/FileUtil.scala
+++ src/cook/util/FileUtil.scala
@@ -4,14 +4,18 @@ import java.io.File
 import java.io.FileNotFoundException
 
 object FileUtil {
-  var root: String = null
 
   val COOK_ROOT_FILENAME = "COOK_ROOT"
 
   def findRootDir(currentDir: File): File = {
-    var dir = currentDir
+    if (root != null) return root
+
+    var dir = currentDir.getAbsoluteFile
     do {
-      if (isRootDir(dir)) return dir
+      if (isRootDir(dir)) {
+        setRoot(dir)
+        return dir
+      }
       dir = dir.getParentFile
     } while (dir != null)
 
@@ -20,6 +24,12 @@ object FileUtil {
   }
 
   private[util]
+  var root: File = null
+
   def isRootDir(dir: File): Boolean = new File(dir, COOK_ROOT_FILENAME).exists
+
+  def setRoot(root: File) {
+    this.root = root.getAbsoluteFile
+  }
 
 }
