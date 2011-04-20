8600f678f6dc4fb7777ff587b77ea9c02bd4d7e7
Tim Green <iamtimgreen@gmail.com>
2011-04-20 22:24:50 +0800

    Impl TargetManager

--- src/cook/target/TargetManager.scala
+++ src/cook/target/TargetManager.scala
@@ -2,6 +2,10 @@ package cook.target
 
 import scala.collection.mutable.HashMap
 
+import cook.config.runner.ConfigType
+import cook.config.runner.CookRunner
+import cook.util.TargetLabel
+
 object TargetManager {
 
   val targets = new HashMap[String, Target]
@@ -10,6 +14,19 @@ object TargetManager {
     targets.put(t.name, t)
   }
 
+  def getTarget(name: String): Target = {
+    if (!hasTarget(name)) {
+      val configFile = new TargetLabel(null, name).config
+      CookRunner.run(configFile, ConfigType.COOK)
+    }
+
+    targets.get(name) match {
+      case Some(target) => target
+      case None => throw new TargetException("Target \"%s\" is not defined".format(name))
+    }
+  }
+
+  private[target]
   def hasTarget(name: String) = targets.contains(name)
 
 }
