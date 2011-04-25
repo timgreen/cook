ba56a75f963f5cd9857a22d8cc71fdf8813c84d7
Tim Green <iamtimgreen@gmail.com>
2011-04-25 23:30:00 +0800

    Impl topological sort in Analysis

--- src/cook/actors/Analysis.scala
+++ src/cook/actors/Analysis.scala
@@ -1,5 +1,6 @@
 package cook.actors
 
+import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashSet
 import scala.collection.mutable.Stack
 import scala.collection.immutable.VectorBuilder
@@ -11,28 +12,64 @@ object Analysis {
 
   def analyze(targetLabel: TargetLabel): Seq[Target] = {
 
-    val targets = new VectorBuilder[Target]
     val labelsProccessing = new Stack[String]
     val labelsProccessingSet = new HashSet[String]
     val labelsProccessed = new HashSet[String]
 
-    analyzeDeps(targetLabel, targets, labelsProccessing, labelsProccessingSet, labelsProccessed)
+    val outDeg = new HashMap[String, Int]
+    val inEdges = new HashMap[String, VectorBuilder[String]]
 
-    targets.result.reverse
+    analyzeDeps(
+        targetLabel,
+        labelsProccessing,
+        labelsProccessingSet,
+        labelsProccessed,
+        outDeg,
+        inEdges)
+
+    val readyToBuild = new Stack[String]
+    val buildOrder = new VectorBuilder[Target]
+    for ((t, i) <- outDeg) {
+      if (i == 0) {
+        readyToBuild.push(t)
+      }
+    }
+
+    while (readyToBuild.nonEmpty) {
+      val t = readyToBuild.pop
+      buildOrder += TargetManager.getTarget(new TargetLabel("", t))
+
+      for (tt <- inEdges.getOrElse(t, new VectorBuilder[String]).result) {
+        val deg = outDeg(tt) - 1
+        if (deg == 0) {
+          readyToBuild.push(tt)
+        } else {
+          outDeg(tt) = deg
+        }
+      }
+    }
+
+    buildOrder.result
   }
 
   def analyzeDeps(
       targetLabel: TargetLabel,
-      targets: VectorBuilder[Target],
       labelsProccessing: Stack[String],
       labelsProccessingSet: HashSet[String],
-      labelsProccessed: HashSet[String]) {
+      labelsProccessed: HashSet[String],
+      outDeg: HashMap[String, Int],
+      inEdges: HashMap[String, VectorBuilder[String]]) {
 
     val target = TargetManager.getTarget(targetLabel)
-    targets += target
     labelsProccessing.push(target.targetName)
     labelsProccessingSet += target.targetName
-    for (dep <- target.depTargets) {
+
+    val depTargets = target.depTargets
+    outDeg.put(target.targetName, depTargets.length)
+
+    for (dep <- depTargets) {
+      inEdges.getOrElseUpdate(dep.targetName, new VectorBuilder[String]) += target.targetName
+
       if (labelsProccessingSet.contains(dep.targetName)) {
         // TODO(timgreen): better error message
         labelsProccessing.push(dep.targetName)
@@ -40,7 +77,7 @@ object Analysis {
             "Found dependence circle: %s", labelsProccessing.mkString(" -> "))
       }
       if (!labelsProccessed.contains(dep.targetName)) {
-        analyzeDeps(dep, targets, labelsProccessing, labelsProccessingSet, labelsProccessed)
+        analyzeDeps(dep, labelsProccessing, labelsProccessingSet, labelsProccessed, outDeg, inEdges)
       }
     }
 
