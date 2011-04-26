5e00af1fd88933c6ad0ed7c4e41bf8acdf8b91b6
Tim Green <iamtimgreen@gmail.com>
2011-04-26 13:10:09 +0800

    Add proguard to reduce jar size

--- build.sh
+++ build.sh
@@ -1,65 +1,6 @@
 #!/bin/sh
 
-ROOT=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-LIB=$ROOT/lib
-SRC=$ROOT/src
-TEST=$ROOT/test
-GEN_DIR=$ROOT/cook_gen
-BUILD_DIR=$ROOT/cook_build
-
-CP=$LIB/Mouse-1.3.jar:$LIB/scala-library.jar:$LIB/ant.jar
-RUNTIM_CP=$LIB/Mouse-1.3.runtime.jar
-SCALA_TEST_CP=$LIB/scalatest-1.3.jar
-
-
-mkBuildDir() {
-  mkdir -p $GEN_DIR
-  mkdir -p $BUILD_DIR
-}
-
-testPeg() {
-  java -cp $CP mouse.TestPEG -G $SRC/cook/config/parser/cook.peg
-}
-
-generateParser() {
-  mkBuildDir
-  mkdir -p $GEN_DIR/cook/config/parser/
-  java -cp $CP mouse.Generate -G $SRC/cook/config/parser/cook.peg -p cook.config.parser -P Parser -S Semantics -D $GEN_DIR/cook/config/parser/
-}
-
-build() {
-  generateParser
-  scala_files=$(find $SRC -name "*.scala" -print)
-  java_files=$(find $GEN_DIR/cook/config/parser/ -name "*.java" -print)
-  scalac -deprecation -unchecked -d $BUILD_DIR -classpath $CP ${scala_files[*]} ${java_files[*]}
-  javac -d $BUILD_DIR -classpath $CP:$BUILD_DIR ${java_files[*]}
-}
-
-buildTest() {
-  build
-  scala_files=$(find $TEST -name "*.scala" -print)
-  scalac -unchecked -d $BUILD_DIR -classpath $CP:$BUILD_DIR:$SCALA_TEST_CP ${scala_files[*]}
-}
-
-runTest() {
-  buildTest
-  java -cp $CP:$BUILD_DIR:$SCALA_TEST_CP org.scalatest.tools.Runner -p . -o -s cook.config.parser.CookParserTest
-  java -cp $CP:$BUILD_DIR:$SCALA_TEST_CP org.scalatest.tools.Runner -p . -o -s cook.config.runner.unit.UnitTest
-}
-
-clear() {
-  rm -fr $GEN_DIR $BUILD_DIR
-}
-
-run() {
-  build
-  java -cp $CP:$BUILD_DIR cook.app.Main build ":cook.all.jar"
-}
-
-main() {
-  #runTest
-  run
-}
-
-main
+./bin/cook build :cook-unprocessed.all.jar
+cd proguard
+ln -sf ../cook_build/COOK_TARGET_cook-unprocessed.all.jar/cook-unprocessed.all.jar cook-unprocessed.jar
+java -Xms512m -Xmx2048m -jar proguard.jar @cook.pro
---
+++ proguard/cook.pro
@@ -0,0 +1,30 @@
+-injars cook-unprocessed.jar
+-outjars cook-processed.jar
+
+-libraryjars <java.home>/lib/rt.jar
+-libraryjars ../lib/ant.jar
+-libraryjars ../lib/Mouse-1.3.jar
+-libraryjars ../lib/scala-library.jar
+
+
+# Allow methods with the same signature, except for the return type,
+# to get the same obfuscation name.
+
+-overloadaggressively
+
+# Put all obfuscated classes into the nameless root package.
+
+-repackageclasses ''
+
+# Allow classes and class members to be made public.
+
+-allowaccessmodification
+
+# The entry point: cook.app.Main and its main method.
+
+-keep public class cook.app.Main {
+    public static void main(java.lang.String[]);
+}
+
+# If you want to preserve the Ant task as well, you'll have to specify the
+# main ant.jar.
