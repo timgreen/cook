8a2438221d12724cd5bd07b62d2dd942d827bb66
Tim Green <iamtimgreen@gmail.com>
2011-04-27 14:08:28 +0800

    Fix EvalException

--- src/cook/config/runner/EvalException.scala
+++ src/cook/config/runner/EvalException.scala
@@ -2,4 +2,4 @@ package cook.config.runner
 
 import cook.util.CookBaseException
 
-class EvalException(error: String, args: Any*) extends CookBaseException(error, args)
+class EvalException(error: String, args: Any*) extends CookBaseException(error, args: _*)
