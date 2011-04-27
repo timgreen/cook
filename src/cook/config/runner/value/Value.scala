5e68a295d6f1c47a0de040fcda84dbe81bf3c501
Tim Green <iamtimgreen@gmail.com>
2011-04-27 12:10:22 +0800

    Refactor value attr

--- src/cook/config/runner/value/Value.scala
+++ src/cook/config/runner/value/Value.scala
@@ -4,12 +4,13 @@ import cook.config.runner.EvalException
 
 abstract class Value(val typeName: String) {
 
-  def attr(id: String): Value
+  def attr(id: String): Value = {
+    throw new EvalException("Unsupportted attr \"%s\" on %s", id, typeName)
+  }
 
   def isNull = false
 
   def ensureNotNull(): Value = ensureNotNull("Null is not allowed in expr")
-
   def ensureNotNull(errorMessage: String): Value = {
     if (isNull) {
       throw new EvalException(errorMessage)
@@ -22,18 +23,11 @@ abstract class Value(val typeName: String) {
 
 case class NullValue() extends Value("Null") {
 
-  override def attr(id: String): Value = {
-    throw new EvalException("Unsupportted attr \"%s\" on Null", id)
-  }
   override def isNull = true
   override def get(): Any = null
 }
 
 case class NumberValue(int: Int) extends Value("Number") {
-
-  override def attr(id: String): Value = id match {
-    case _ => throw new EvalException("attr \"%s\" is not supportted by Number", id)
-  }
 
   override def get(): Any = int
 }
@@ -43,7 +37,7 @@ case class StringValue(str: String) extends Value("String") {
   override def attr(id: String): Value = id match {
     case "size" => NumberValue(str.size)
     case "length" => NumberValue(str.length)
-    case _ => throw new EvalException("attr \"%s\" is not supportted by String", id)
+    case _ => super.attr(id)
   }
 
   override def get(): Any = str
@@ -54,7 +48,7 @@ case class ListValue(list: Seq[Value]) extends Value("List") {
   override def attr(id: String): Value = id match {
     case "size" => NumberValue(list.size)
     case "length" => NumberValue(list.length)
-    case _ => throw new EvalException("attr \"%s\" is not supportted by List", id)
+    case _ => super.attr(id)
   }
 
   override def get(): Any = list
