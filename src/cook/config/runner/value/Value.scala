ef76a1bae745cc4882c34f4f199219b574048a48
Tim Green <iamtimgreen@gmail.com>
2011-05-04 22:05:48 +0800

    Impl Value.toString & Value.tos

--- src/cook/config/runner/value/Value.scala
+++ src/cook/config/runner/value/Value.scala
@@ -1,26 +1,33 @@
 package cook.config.runner.value
 
+import scala.collection.mutable.StringBuilder
+
 import cook.config.runner.EvalException
 
 abstract class Value(val typeName: String) {
 
-  def attr(id: String): Value = {
-    throw new EvalException("Unsupportted attr \"%s\" on %s", id, typeName)
+  def attr(id: String): Value = id match {
+    case "tos" => StringValue(this.toString)
+    case _ => throw new EvalException("Unsupportted attr \"%s\" on %s", id, typeName)
   }
 
   def isNull = false
   def get(): Any
+
+  def toString(): String
 }
 
 case class NullValue() extends Value("Null") {
 
   override def isNull = true
   override def get(): Any = null
+  override def toString(): String = "null"
 }
 
 case class BooleanValue(bool: Boolean) extends Value("Bool") {
 
   override def get(): Any = bool
+  override def toString(): String = bool.toString
 }
 object BooleanValue {
 
@@ -31,6 +38,7 @@ object BooleanValue {
 case class NumberValue(int: Int) extends Value("Number") {
 
   override def get(): Any = int
+  override def toString(): String = int.toString
 }
 
 case class StringValue(str: String) extends Value("String") {
@@ -44,6 +52,7 @@ case class StringValue(str: String) extends Value("String") {
   }
 
   override def get(): Any = str
+  override def toString(): String = str
 }
 
 case class ListValue(list: Seq[Value]) extends Value("List") {
@@ -57,6 +66,11 @@ case class ListValue(list: Seq[Value]) extends Value("List") {
   }
 
   override def get(): Any = list
+  override def toString(): String = {
+    val builder = new StringBuilder
+    list.addString(builder, "[", ", ", "]")
+    builder.result
+  }
 }
 
 object ListValue {
