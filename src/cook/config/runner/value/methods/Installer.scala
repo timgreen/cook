6f542fc8cb7591bf1e27b47123535e889926ea89
Tim Green <iamtimgreen@gmail.com>
2011-05-05 12:57:36 +0800

    Impl string.contains("subtring")

--- src/cook/config/runner/value/methods/Installer.scala
+++ src/cook/config/runner/value/methods/Installer.scala
@@ -3,7 +3,11 @@ package cook.config.runner.value.methods
 object Installer {
 
   def install {
+    // list
     ValueMethod.listMethods.put("join", Join)
     ValueMethod.listMethods.put("mkString", Join)
+
+    // string
+    ValueMethod.stringMethods.put("contains", Contains)
   }
 }
---
+++ src/cook/config/runner/value/methods/string/Contains.scala
@@ -0,0 +1,32 @@
+package cook.config.runner.value.methods
+
+import scala.collection.mutable.HashMap
+
+import cook.config.parser.unit._
+import cook.config.runner.Scope
+import cook.config.runner.unit._
+import cook.config.runner.value._
+
+object ContainsArgsDef {
+
+  def apply(): ArgsDef = {
+    val names = Seq[String]("substring")
+    val defaultValues = new HashMap[String, Value]
+    new ArgsDef(names, defaultValues)
+  }
+}
+
+class Contains(v: Value, name: String) extends ValueMethod(v, name, ContainsArgsDef()) {
+
+  override def run(path: String, argsValue: ArgsValue): Value = {
+    val s = getStringOrError(Some(v))
+    val sub = getStringOrError(argsValue.get("substring"))
+
+    BooleanValue(s.contains(sub))
+  }
+}
+
+object Contains extends ValueMethodBuilder {
+
+  def apply(v: Value, name: String): ValueMethod = new Contains(v, name)
+}
