5282c049978a4849942b98c84bd8ff35097c8283
Tim Green <iamtimgreen@gmail.com>
2011-05-19 11:04:04 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 ./bin/cook build :cook-unprocessed.all.jar
 if (( $? == 0 )); then
   cd proguard
-  ln -sf ../cook_build/COOK_TARGET_cook-unprocessed.all.jar/cook-unprocessed.all.jar cook-unprocessed.jar
+  ln -sf ../cook_build/COOK_TARGET_BUILD_cook-unprocessed.all.jar/cook-unprocessed.all.jar cook-unprocessed.jar
   java -Xms512m -Xmx2048m -jar proguard.jar @cook-standalone.pro
 fi
