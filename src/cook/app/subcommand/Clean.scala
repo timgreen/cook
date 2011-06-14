50b73cfff37a312c44740db74e5f5e21b10b064c
Tim Green <iamtimgreen@gmail.com>
2011-06-14 11:36:15 +0800

    Support only clean target

--- src/cook/app/subcommand/Clean.scala
+++ src/cook/app/subcommand/Clean.scala
@@ -3,17 +3,38 @@ package cook.app.subcommand
 import java.io.File
 
 import cook.app.console.CookConsole
+import cook.target._
 import cook.util._
 
 object Clean extends SubCommand("clean", "Clean up cook output") {
 
   override def run(args: Array[String]): Int = {
-    deleteRecursively(FileUtil.cookBuildDir)
+    if (args.nonEmpty) {
+      // TODO(timgreen): move to better place
+      val currentDir = FileUtil.relativeDirToRoot(System.getProperty("user.dir"))
+
+      for (a <- args) {
+        val targetLabel = new TargetLabel(currentDir, a)
+        val target = TargetManager.getTarget(targetLabel)
+        CookConsole.print("Cleaning target \"%s\" ... ", target.targetName)
+
+        deleteRecursively(target.outputDir)
+        deleteRecursively(target.buildLogFile)
+        deleteRecursively(target.buildShFile)
+        deleteRecursively(target.runLogFile)
+        deleteRecursively(target.runShFile)
+
+        CookConsole.println("Done")
+      }
+    } else {
+      deleteRecursively(FileUtil.cookBuildDir)
+    }
     0
   }
 
   def help() {
     CookConsole.println("clean cook output")
+    CookConsole.println("Usage: clean [<target name(s) ...>]")
   }
 
   private[subcommand]
