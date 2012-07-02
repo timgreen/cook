173a642c51eddf70ad40b1cc286121245290a824
Tim Green <iamtimgreen@gmail.com>
2012-07-02 23:39:07 +0800

    Add PathUtil

---
+++ src/cook/util/PathUtil.scala
@@ -0,0 +1,39 @@
+package cook.util
+
+import cook.error.ErrorMessageHandler
+
+import scala.annotation.tailrec
+import scala.tools.nsc.io.Directory
+import scala.tools.nsc.io.Path
+
+
+object PathUtil extends ErrorMessageHandler {
+
+  private var cookRootOption: Option[Directory] = None
+  def findRootDir(currentDir: Directory): Directory = cookRootOption match {
+    case Some(r) => r
+    case None =>
+      cookRootOption = findRootDirInternal(currentDir)
+      if (!cookRootOption.isDefined) {
+        reportError("Can not find Cook Build Root, from dir %s".format(currentDir.toString))
+      }
+      cookRootOption.get
+  }
+
+  @tailrec
+  private def findRootDirInternal(dir: Directory): Option[Directory] = if (isCookRoot(dir)) {
+    Some(dir)
+  } else if (dir.isRootPath) {
+    None
+  } else {
+    findRootDirInternal(dir.parent)
+  }
+
+  private def isCookRoot(dir: Directory) = (dir / "COOK_ROOT").canRead
+
+  lazy val cookRoot = cookRootOption.get
+  lazy val cookBuildDir = (cookRoot / ".cook_build").toDirectory
+  lazy val cookConfigScalaSourceDir = (cookBuildDir / "config_srcs").toDirectory
+  lazy val cookConfigClassDir = (cookBuildDir / "config_classes").toDirectory
+  lazy val cookTargetBuildDir = (cookBuildDir / "targets").toDirectory
+}
