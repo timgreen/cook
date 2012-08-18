5c7adc8d58a4da6b232404220ae6a50708147189
Tim Green <iamtimgreen@gmail.com>
2012-08-18 21:26:38 +0800

    Add test for glob command

--- src/cook/config/dsl/buildin/Glob.scala
+++ src/cook/config/dsl/buildin/Glob.scala
@@ -16,13 +16,23 @@ trait Glob {
     ) toList
   }
 
+  def glob(includes: Seq[String], excludes: Seq[String])
+    (implicit context: ConfigContext): List[Path] = {
+    GlobScanner(
+      dir = context.path,
+      includes = includes,
+      excludes = excludes,
+      fileOnly = true
+    ) toList
+  }
+
   def glob(includes: Seq[String], baseDir: Directory,
-    excludes: Seq[String] = Seq(), fileOnly: Boolean = true) = {
+    excludes: Seq[String] = Seq(), fileOnly: Boolean = true): List[Path] = {
     GlobScanner(
       dir = baseDir,
       includes = includes,
       excludes = excludes,
       fileOnly = fileOnly
-    )
+    ) toList
   }
 }
--- test/cook/config/dsl/BuildinTest.scala
+++ test/cook/config/dsl/BuildinTest.scala
@@ -4,6 +4,7 @@ import cook.config.Config
 import cook.config.ConfigCompiler
 import cook.config.ConfigGenerator
 import cook.config.ConfigLoader
+import cook.config.ConfigEngine
 import cook.config.ConfigRef
 import cook.config.testing.ConfigRefTestHelper
 import cook.path.testing.PathUtilHelper
@@ -16,7 +17,7 @@ import scala.tools.nsc.io.Directory
 import scala.tools.nsc.io.Path
 
 
-class GenTargetTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
+class BuildinTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
 
   before {
     ClassPathBuilderHelper.reset(ConfigCompiler.cpBuilder)
@@ -60,5 +61,37 @@ class GenTargetTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
     val t2 = ts("target_defg")
     t2.build
     t2.result.asInstanceOf[{ def length: Int}].length should be (4)
+  }
+
+  "Glob" should "scan current dir" in {
+    fakePath("test1")
+
+    val r = ConfigRef(List("a", "b", "c", "COOK"))
+    val s = ConfigRef(List("rules", "string.cooki"))
+
+    val c = ConfigEngine.load(r).asInstanceOf[{
+      def a: List[Path]
+      def b: List[Path]
+      def c: List[Path]
+    }]
+
+    c.a map { _.name } should be (List("aaa", "abc"))
+    c.b map { _.name } should be (List("bbb", "abc"))
+    c.c map { _.name } should be (List("abc"))
+  }
+
+  it should "support excludes" in {
+    fakePath("test1")
+
+    val r = ConfigRef(List("a", "b", "c", "COOK"))
+    val s = ConfigRef(List("rules", "string.cooki"))
+
+    val c = ConfigEngine.load(r).asInstanceOf[{
+      def a2: List[Path]
+      def b2: List[Path]
+    }]
+
+    c.a2 map { _.name } should be (List("abc"))
+    c.b2 map { _.name } should be (List("bbb"))
   }
 }
