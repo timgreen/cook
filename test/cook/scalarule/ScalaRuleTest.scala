72cf51e096819773c34fbc617e7f7771c469401a
Tim Green <iamtimgreen@gmail.com>
2012-08-21 11:41:49 +0800

    Add test for scalarule.

---
+++ test/cook/scalarule/ScalaRuleTest.scala
@@ -0,0 +1,43 @@
+package cook.scalarule
+
+import cook.config.ConfigCompiler
+import cook.config.ConfigEngine
+import cook.config.ConfigGenerator
+import cook.config.ConfigLoader
+import cook.config.ConfigRef
+import cook.config.testing.ConfigRefTestHelper
+import cook.path.testing.PathUtilHelper
+import cook.util.testing.ClassPathBuilderHelper
+
+import org.scalatest.BeforeAndAfter
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import scala.tools.nsc.io.Directory
+import scala.tools.nsc.io.Path
+
+class ScalaRuleTest extends FlatSpec with ShouldMatchers with BeforeAndAfter {
+
+  before {
+    ClassPathBuilderHelper.reset(ConfigCompiler.cpBuilder)
+    ConfigCompiler.initDefaultCp
+    fakePath("test1")
+  }
+
+  def fakePath(dirname: String) {
+    val dir = Directory("testoutput/" + dirname)
+    dir.deleteRecursively
+
+    val sourceDir = dir / "scala"
+    val bytecodeDir = dir / "bytecode"
+    PathUtilHelper.fakePath(
+      cookRootOption = Some((Path("testdata") / dirname) toDirectory),
+      cookConfigScalaSourceDirOption = Some(sourceDir toDirectory),
+      cookConfigByteCodeDirOption = Some(bytecodeDir toDirectory)
+    )
+  }
+
+  "ScalaRule" should "work" in {
+    val r = ConfigRef(List("COOK"))
+    val c = ConfigEngine.load(r)
+  }
+}
