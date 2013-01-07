2d1296c4c071d3a9cfe2d118125c08fe88b1a815
Tim Green <iamtimgreen@gmail.com>
2013-01-07 09:18:40 +0800

    Skip jar download if file already exists.

--- travis.sh
+++ travis.sh
@@ -7,9 +7,9 @@ cd tmp
 ## prepare scala home
 mkdir -p scala
 cd scala
-wget -c http://www.scala-lang.org/downloads/distrib/files/scala-$TRAVIS_SCALA_VERSION.tgz
-rm -fr scala-$TRAVIS_SCALA_VERSION
-tar xf scala-$TRAVIS_SCALA_VERSION.tgz
+[ -r scala-$TRAVIS_SCALA_VERSION ] || (wget -c http://www.scala-lang.org/downloads/distrib/files/scala-$TRAVIS_SCALA_VERSION.tgz &&
+  rm -fr scala-$TRAVIS_SCALA_VERSION &&
+  tar xf scala-$TRAVIS_SCALA_VERSION.tgz)
 export SCALA_HOME=$PWD/scala-$TRAVIS_SCALA_VERSION
 ln -sf $SCALA_HOME/lib/scala-compiler.jar ../../lib/
 ln -sf $SCALA_HOME/lib/scala-library.jar ../../lib/
@@ -21,10 +21,12 @@ cd ..
 ## prepare lib
 mkdir -p lib
 cd lib
-wget -c https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.10.0-RC5/2.0.M5-B1/scalatest_2.10.0-RC5-2.0.M5-B1.jar
-ln -sf $PWD/scalatest_2.10.0-RC5-2.0.M5-B1.jar ../../lib/scalatest.jar
-wget -c http://repo.typesafe.com/typesafe/repo/org/rogach/scallop_2.10/0.6.4/scallop_2.10-0.6.4.jar
-ln -sf $PWD/scallop_2.10-0.6.4.jar ../../lib/scallop.jar
+SCALA_TEST_JAR=scalatest_2.10.0-RC5-2.0.M5-B1.jar
+[ -r $SCALA_TEST_JAR ] || (wget -c https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.10.0-RC5/2.0.M5-B1/$SCALA_TEST_JAR &&
+  ln -sf $PWD/$SCALA_TEST_JAR ../../lib/scalatest.jar)
+SCALLOP_JAR=scallop_2.10-0.6.4.jar
+[ -r $SCALLOP_JAR ] || (wget -c http://repo.typesafe.com/typesafe/repo/org/rogach/scallop_2.10/0.6.4/$SCALLOP_JAR
+  ln -sf $PWD/$SCALLOP_JAR ../../lib/scallop.jar)
 cd ..
 
 ## build & test
