ae628660a579eb9ab21fed5eea72ac39ddce6756
Tim Green <iamtimgreen@gmail.com>
2013-06-09 17:32:45 +0800

    Impl BerkeleyDbImpl.

--- src/cook/meta/Meta.scala
+++ src/cook/meta/Meta.scala
@@ -7,9 +7,19 @@ class Meta extends mutable.HashMap[String, String] {
   def add(group: String, key: String, value: String) {
     this(group + "|" + key) = value
   }
+
+  def toBytes: Array[Byte] = Meta.toBytes(this)
 }
 
 object Meta {
 
-  def apply = new Meta
+  def toBytes(meta: Meta): Array[Byte] = {
+    // TODO(timgreen):
+    null
+  }
+
+  def fromBytes(bytes: Array[Byte]): Meta = {
+    // TODO(timgreen):
+    null
+  }
 }
--- src/cook/meta/db/BerkeleyDbImpl.scala
+++ src/cook/meta/db/BerkeleyDbImpl.scala
@@ -5,19 +5,23 @@ import cook.path.Path
 
 import com.sleepycat.je.Database
 import com.sleepycat.je.DatabaseConfig
+import com.sleepycat.je.DatabaseEntry
 import com.sleepycat.je.Environment
 import com.sleepycat.je.EnvironmentConfig
+import com.sleepycat.je.LockMode
+import com.sleepycat.je.OperationStatus
 
 object BerkeleyDbImpl extends Db {
 
   private var _db: Database = _
 
-  def dbFile = Path().metaDbFile.jfile
+  def dbFile = Path().metaDbFile
 
   override def open {
     val envConf = new EnvironmentConfig
     envConf.setAllowCreate(true)
-    val dbEnv = new Environment(dbFile, envConf)
+    dbFile.parent.createDirectory()
+    val dbEnv = new Environment(dbFile.jfile, envConf)
     val dbConf = new DatabaseConfig
     dbConf.setAllowCreate(true)
     _db = dbEnv.openDatabase(null, "meta", dbConf)
@@ -27,12 +31,23 @@ object BerkeleyDbImpl extends Db {
     _db.close
   }
 
+  private implicit def string2databaseEntry(s: String) =
+    new DatabaseEntry(s.getBytes("UTF-8"))
+
+  private implicit def meta2databaseEntry(meta: Meta) =
+    new DatabaseEntry(meta.toBytes)
+
   override def get(key: String): Meta = {
-    // TODO(timgreen):
-    null
+    val data = new DatabaseEntry
+    _db.get(null, key, data, LockMode.DEFAULT) match {
+      case OperationStatus.SUCCESS =>
+        Meta.fromBytes(data.getData)
+      case _ =>
+        new Meta
+    }
   }
 
   override def put(key: String, meta: Meta) {
-    // TODO(timgreen):
+    _db.put(null, key, meta)
   }
 }
