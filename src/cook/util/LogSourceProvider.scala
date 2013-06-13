0047c3a91d474cecba771fdfd8cdbc60de59aa8c
Tim Green <iamtimgreen@gmail.com>
2013-06-13 10:40:17 +0800

    Add LogSourceProvider.

---
+++ src/cook/util/LogSourceProvider.scala
@@ -0,0 +1,11 @@
+package cook.util
+
+import akka.event.LogSource
+
+object LogSourceProvider {
+
+  implicit val logSource: LogSource[AnyRef] = new LogSource[AnyRef] {
+    def genString(o: AnyRef): String = o.getClass.getName
+    override def getClazz(o: AnyRef): Class[_] = o.getClass
+  }
+}
