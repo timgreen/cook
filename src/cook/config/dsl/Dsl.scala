6cf49a4f29d4be5a1e142611f91fd3fb640ca46e
Tim Green <iamtimgreen@gmail.com>
2013-06-14 16:19:00 +0800

    Export target related type in Dsl.

--- src/cook/config/dsl/Dsl.scala
+++ src/cook/config/dsl/Dsl.scala
@@ -4,6 +4,10 @@ import cook.config.dsl.buildin._
 
 object Dsl extends BuildinCommands {
 
+  type TargetResult = cook.target.TargetResult
+  type Target[R <: TargetResult] = cook.target.Target[R]
+  type UnitResult = cook.target.UnitResult
+
   val UnitResult    = cook.target.UnitResult
   val UnitResultFn  = cook.target.UnitResultFn
   val EmptyBuildCmd = cook.target.EmptyBuildCmd
