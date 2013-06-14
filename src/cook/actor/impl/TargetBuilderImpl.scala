bbe4cc76add8bd0dedfd1887d182358bd3e7869f
Tim Green <iamtimgreen@gmail.com>
2013-06-14 11:38:11 +0800

    Impl TargetBuilderImpl.

--- src/cook/actor/impl/TargetBuilderImpl.scala
+++ src/cook/actor/impl/TargetBuilderImpl.scala
@@ -2,6 +2,7 @@ package cook.actor.impl
 
 import cook.actor.TargetBuilder
 import cook.actor.impl.util.BatchResponser
+import cook.app.Global
 import cook.ref.TargetRef
 import cook.target.Target
 import cook.target.TargetResult
@@ -11,6 +12,16 @@ import akka.actor.TypedActor
 import scala.collection.mutable
 import scala.concurrent.{ Promise, Future, Await }
 import scala.util.{ Try, Success, Failure }
+
+
+object TargetBuildTask {
+
+  def apply(refName: String)(runBlock: => Unit): Runnable = new Runnable {
+    override def run() {
+      runBlock
+    }
+  }
+}
 
 
 /**
@@ -24,14 +35,15 @@ class TargetBuilderImpl extends TargetBuilder with TypedActorBase {
 
   private val responser = new BatchResponser[String, TargetResult]()
   private val dagSolver = new DagSolver
-  private val builtTargetNames = mutable.Set[String]()
-  private val pendingTargets = mutable.Map[String, Target[_]]()
+  private val pendingTargets = mutable.Map[String, Target[TargetResult]]()
   private def self = targetBuilder
 
   override def taskComplete(refName: String)(tryTargetResult: Try[TargetResult]) {
     responser.complete(refName)(tryTargetResult)
-    dagSolver.markDone(refName)
-    self.checkDag
+    if (tryTargetResult.isSuccess) {
+      dagSolver.markDone(refName)
+      self.checkDag
+    }
   }
 
   override def build(targetRef: TargetRef): Future[TargetResult] = {
@@ -48,34 +60,42 @@ class TargetBuilderImpl extends TargetBuilder with TypedActorBase {
       case Success(t) =>
         self.step2WaitForDeps(t)
       case Failure(e) =>
+        log.debug("step1GetTarget.failure {}", e)
         self.taskComplete(targetRef.refName)(Failure(e))
     }
   }
 
   override def step2WaitForDeps(target: Target[TargetResult]) {
-    if (builtTargetNames.contains(target.refName)) {
-      self.taskComplete(target.refName)(Try(target.result))
-    } else {
-      pendingTargets(target.refName) = target
-      dagSolver.addDeps(target.refName, target.deps.map(_.refName))
-      target.deps foreach self.build
-      self.checkDag
+    pendingTargets(target.refName) = target
+    dagSolver.addDeps(target.refName, target.deps.map(_.refName))
+    import TypedActor.dispatcher
+    target.deps foreach { depTargetRef =>
+      self.build(depTargetRef) onFailure {
+        case e =>
+          self.taskComplete(target.refName)(Failure(e))
+      }
     }
-  }
-
-  override def step3BuildTarget(targetName: String) {
-    val target = pendingTargets.remove(targetName)
-    // TODO(timgreen):
-    // target.build
-    // ...
-    // self.onComplete(targetName)(...)
-    // builtTargetNames += targetName
+    self.checkDag
   }
 
   override def checkDag {
     if (dagSolver.hasAvaliable) {
       val avaliableTargetName = dagSolver.pop
       self.step3BuildTarget(avaliableTargetName)
+    }
+  }
+
+  override def step3BuildTarget(targetName: String) {
+    pendingTargets.remove(targetName) match {
+      case Some(target) =>
+        Global.workerDispatcher.execute(TargetBuildTask(targetName) {
+          self.taskComplete(targetName)(Try {
+            target.build
+            target.result
+          })
+        })
+      case None =>
+        // TODO(timgreen): this should never happen, report error
     }
   }
 }
