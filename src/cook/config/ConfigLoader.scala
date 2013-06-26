540931c166dca75e510ae796ca840d8fad5074aa
Tim Green <iamtimgreen@gmail.com>
2013-06-26 22:40:02 +0800

    Use same class loader contexts for all config.

--- src/cook/config/ConfigLoader.scala
+++ src/cook/config/ConfigLoader.scala
@@ -1,19 +1,22 @@
 package cook.config
 
+import java.lang.ClassLoader
 import java.net.URLClassLoader
 
 
 object ConfigLoader {
 
-  def load(configRef: ConfigRef, depConfigRefMap: Map[String, ConfigRef]): Config = {
+  def load(configRef: ConfigRef, depConfigRefMap: Map[String, ConfigRef]): Config = synchronized {
     val cl = getClassLoader(configRef, depConfigRefMap)
     val clazz = cl.loadClass(configRef.configClassFullName)
     clazz.asInstanceOf[Class[Config]].newInstance
   }
 
+  private var cl: ClassLoader = this.getClass.getClassLoader
   private def getClassLoader(configRef: ConfigRef, depConfigRefMap: Map[String, ConfigRef]) = {
     val depConfigRefs = depConfigRefMap.values.toList
     val cp = (configRef :: depConfigRefs).map(_.configByteCodeDir.toURI.toURL).toArray
-    new URLClassLoader(cp, this.getClass.getClassLoader)
+    cl = new URLClassLoader(cp, cl)
+    cl
   }
 }
