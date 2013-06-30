88ffe58de94ca8494e76fab4ec790948170df1f8
Tim Green <iamtimgreen@gmail.com>
2013-06-30 14:22:44 +0800

    Make cmd in rules configable.

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COOK_VERSION=v1.0.0-M1
+COOK_VERSION=v1.0.0-M2
 
 mkdir -p lib
 mkdir -p tmp
@@ -16,6 +16,13 @@ cd scala
 rm -f scala
 ln -s scala-$TRAVIS_SCALA_VERSION scala
 
+link() {
+  [[ $(readlink -f "$1") != $(readlink -f "$2") ]] && {
+    rm -f "$2"
+    ln -sf "$1" "$2"
+  }
+}
+
 export SCALA_HOME=$PWD/scala-$TRAVIS_SCALA_VERSION
 ln -sf $SCALA_HOME/lib/scala-compiler.jar ../../lib/
 ln -sf $SCALA_HOME/lib/scala-library.jar ../../lib/
@@ -28,22 +35,22 @@ cd ..
 mkdir -p lib
 cd lib
 SCALA_TEST_JAR=scalatest_2.10.0-2.0.M5.jar
-[ -r $SCALA_TEST_JAR ] || (wget -c https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.10.0/2.0.M5/$SCALA_TEST_JAR &&
-  ln -sf $PWD/$SCALA_TEST_JAR ../../lib/scalatest.jar)
+[ -r $SCALA_TEST_JAR ] || wget -c https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.10.0/2.0.M5/$SCALA_TEST_JAR
+link $PWD/$SCALA_TEST_JAR ../../lib/scalatest.jar
 SCALLOP_JAR=scallop_2.10-0.6.4.jar
-[ -r $SCALLOP_JAR ] || (wget -c http://repo.typesafe.com/typesafe/repo/org/rogach/scallop_2.10/0.6.4/$SCALLOP_JAR &&
-  ln -sf $PWD/$SCALLOP_JAR ../../lib/scallop.jar)
+[ -r $SCALLOP_JAR ] || wget -c http://repo.typesafe.com/typesafe/repo/org/rogach/scallop_2.10/0.6.4/$SCALLOP_JAR
+link $PWD/$SCALLOP_JAR ../../lib/scallop.jar
 JE_JAR=je-3.2.76.jar
-[ -r $JE_JAR ] || (wget -c http://repo1.maven.org/maven2/berkeleydb/je/3.2.76/$JE_JAR &&
-  ln -sf $PWD/$JE_JAR ../../lib/je.jar)
+[ -r $JE_JAR ] || wget -c http://repo1.maven.org/maven2/berkeleydb/je/3.2.76/$JE_JAR
+link $PWD/$JE_JAR ../../lib/je.jar
 cd ..
 
 ## prepare cook
 mkdir -p cook
 cd cook
 COOK_JAR=cook-$COOK_VERSION.jar
-[ -r $COOK_JAR ] || (wget -c http://downloads.sourceforge.net/project/cook-bin/$COOK_JAR &&
-  ln -sf $PWD/$COOK_JAR ../../bin/cook.jar)
+[ -r $COOK_JAR ] || wget -c http://downloads.sourceforge.net/project/cook-bin/$COOK_JAR
+link $PWD/$COOK_JAR ../../bin/cook.jar
 cd ..
 
 ## build & test
